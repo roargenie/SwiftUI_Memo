b6324ad8e6a6b2a6868fa7593f2902081467cf09
roargenie <lazyhoper987@gmail.com>
2023-04-01 15:06:39 +0900

    [Init] Project Setting

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 이명진 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
