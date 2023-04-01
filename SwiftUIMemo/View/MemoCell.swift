211f64b69b62f5177c9e7727cdc263454e311310
roargenie <lazyhoper987@gmail.com>
2023-04-01 15:33:43 +0900

    [Chore] MainListView ListRow뷰로 나누어 구성

---
+++ SwiftUIMemo/View/MemoCell.swift
@@ -0,0 +1,31 @@
+//
+//  MemoCell.swift
+//  SwiftUIMemo
+//
+//  Created by 이명진 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo: Memo
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text(memo.insertDate, style: .date)
+                .font(.caption)
+                .foregroundColor(.secondary)
+            
+        }
+    }
+}
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Test"))
+    }
+}
