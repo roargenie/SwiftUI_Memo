c62105fd1554b525cf498f02ba8a1faeef7b329d
roargenie <lazyhoper987@gmail.com>
2023-04-01 15:26:24 +0900

    [Chore] MainListView List DataBinding

--- SwiftUIMemo/SwiftUIMemoApp.swift
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    // 여러 뷰에서 하나의 하나의 데이터를 공유하기 위해 사용
+    @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
